d4d4b452ab218b14e2b96bc0349f3835d2223e34
Jukka Dahlbom <jdahlbom@protonmail.com>
2019-11-15 22:02:37 +0200

    [dynamic trays] Toothing, non-inverted working

--- generate-svg-from-columns.py
+++ generate-svg-from-columns.py
@@ -9,6 +9,47 @@ STROKE = 0.1
 def get_drawing(result_file_name, width='400mm', height='300mm'):
     return svgwrite.Drawing(result_file_name, height=height, width=width, viewBox=(0, 0, 400, 300))
 
+def generate_toothing(direction, path, invert, length, edge_w):
+    #Directions:
+    #0 - right
+    #1 - down
+    #2 - left
+    #3 - up
+
+    def axis(clockwise_rot):
+        move_dir = (clockwise_rot + direction) % 4
+        if move_dir % 2 == 1:
+            return 'v'
+        else:
+            return 'h'
+
+    def dir_value(clockwise_rot, abs_value):
+        rot_dir = (clockwise_rot + direction) % 4
+
+        if rot_dir<2 and invert:
+            return (0-abs_value)
+        elif rot_dir>1 and not invert:
+            return (0-abs_value)
+        return abs_value
+
+
+    if length < 30:
+        raise Exception("Edge too short for toothing: {}".format(length))
+
+    divisions = int(length / 10)
+    if divisions % 2 == 0:
+        divisions -= 1
+    if divisions > 9:
+        divisions = 9
+        
+    for idx in range(0, divisions):
+        if idx % 2 == 0:
+            path.push('{} {}'.format(axis(0), dir_value(0, length/divisions)))
+        else:
+            path.push('{} {}'.format(axis(1), dir_value(1, edge_w)))
+            path.push('{} {}'.format(axis(0), dir_value(0, length/divisions)))
+            path.push('{} {}'.format(axis(3), dir_value(3, edge_w)))
+
 
 def generate_edges(dwg, trayspec):
     spacer_w = trayspec['spacer_width']
@@ -31,21 +72,16 @@ def generate_edges(dwg, trayspec):
             path.push('v -{}'.format(INDENT_DEPTH))
     path.push('h {}'.format(edge_w))
     #Right edge
-    path.push('v {}'.format(depth/3))
-    path.push('h -{}'.format(edge_w))
-    path.push('v {}'.format(depth/3))
-    path.push('h {}'.format(edge_w))
-    path.push('v {}'.format(depth/3))
+    generate_toothing(1, path, False, depth, edge_w)
     path.push('v {}'.format(edge_w))
     #Bottom edge
-    path.push('h -{}'.format(trayspec['tray_width']))
+    content_width = trayspec['tray_width'] - edge_w*2
+    path.push('h -{}'.format(edge_w))
+    generate_toothing(2, path, False, content_width, edge_w)
+    path.push('h -{}'.format(edge_w))
     #Left edge
     path.push('v -{}'.format(edge_w))
-    path.push('v -{}'.format(depth/3))
-    path.push('h {}'.format(edge_w))
-    path.push('v -{}'.format(depth/3))
-    path.push('h -{}'.format(edge_w))
-    path.push('v -{}'.format(depth/3))
+    generate_toothing(3, path, False, depth, edge_w)
     path.push('z')
 
     return [path]
@@ -68,7 +104,6 @@ if __name__ == '__main__':
         sys.exit(1)
 
     tray_defs = generate_columns_from_spec(spacer_width=2, edge_width=3.5, specsfile=sys.argv[1])
-    print(tray_defs)
 
     dwg = get_drawing('test-drawing.svg')
     draw(dwg, tray_defs)
